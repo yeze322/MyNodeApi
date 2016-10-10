3e3ba76c0b6ff51e215ce4f09498dc651998f4eb
Ze Ye <zeye@microsoft.com>
2016-10-10 17:26:52 +0800

    apply cookie parser to manage cookies

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser  = require('cookie-parser')
+var sha1 = require('sha1')
 
 var app = express();
 app.use(bodyParser.json())
+app.use(cookieParser())
 
-var data = {
+var globalState = {
   'name' : 'yeze322',
   'repo' : 'MyNodeApi',
   'pswd' : '123456'
@@ -16,19 +19,19 @@ var repo = 'MyNodeApi'
 var registerKey = (field) => {
   var url = '/' + field
   app.get(url, function(req, res) {
-    if(data[field] === undefined) {
+    if(globalState[field] === undefined) {
       res.status(204)
     }
     res.setHeader('Access-Control-Allow-Origin', '*')
-    res.send(data[field])
+    res.send(globalState[field])
   })
   app.post(url, function(req, res) {
     if(req.body[field] === undefined) {
       res.status(404)
       res.send(`Not received ${field}! Error!`)
     }else{
-      res.send('received: ' + data[field] + '->' + req.body[field])
-      data[field] = req.body[field]
+      res.send('received: ' + globalState[field] + '->' + req.body[field])
+      globalState[field] = req.body[field]
     }
   })
 }
@@ -37,14 +40,12 @@ registerKey('name')
 registerKey('repo')
 registerKey('pswd')
 
-var lastCookie = ''
-
 var TrustSiteDic = {
   "http://localhost:3222": true,
   "http://yeze.eastasia.cloudapp.azure.com:3222": true
 }
 
-var AJAX_CORS_CHECK = (req, res) => {
+var CHECK_ORIGIN_TRUST = (req, res) => {
   if (req.headers.origin in TrustSiteDic) {
     // enable AJAX CORS for trust sites
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
@@ -54,18 +55,30 @@ var AJAX_CORS_CHECK = (req, res) => {
   return false
 }
 
+var lastCookie = ''
+
+var CHECK_COOKIE_EXIST = (req) => {
+  console.log(req.cookies)
+  return req.cookies.uatoken === lastCookie
+}
+
+var CHECK_LOGIN = (req, res) => {
+  var checked = req.query.name === globalState.name && req.query.pswd === globalState.pswd
+  if(checked){
+    lastCookie = sha1(req.query.name + '|' + req.query.pswd)
+    res.cookie('uatoken', lastCookie)
+  }
+  return checked
+}
+
 app.get('/login', function(req, res) {
-  if (AJAX_CORS_CHECK(req, res)) {
+  if (CHECK_ORIGIN_TRUST(req, res)) {
     console.log(req.headers.cookie + '|' + lastCookie)
-    // check cookies
-    if (req.headers.cookie === lastCookie) {
+    if (CHECK_COOKIE_EXIST(req)) {
       res.send(true)
     }else{
-      var checked = req.query.name === data.name && req.query.pswd === data.pswd
-      if(checked){
-        lastCookie = req.headers.cookie
-      }
-      res.send(checked)
+      var responseObj = CHECK_LOGIN(req, res)
+      res.send(responseObj)
     }
   }else{
     res.status(403)
@@ -74,7 +87,7 @@ app.get('/login', function(req, res) {
 })
 
 app.post('/logout', function(req, res) {
-  if (AJAX_CORS_CHECK(req, res)) {
+  if (CHECK_ORIGIN_TRUST(req, res)) {
     lastCookie = ""
     res.send('cookies clear')
   }else{
@@ -86,6 +99,9 @@ app.post('/logout', function(req, res) {
 
 
 app.get('/supervisor', function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.cookie('sptest', sha1(Math.random()))
   res.send(req.headers)
 })
 
