074b413fdebac33ffb0310175d5a8397299c49ac
Ze Ye <zeye@microsoft.com>
2016-10-17 11:47:33 +0800

    check cookie with redis

--- api/login.js
+++ api/login.js
@@ -1,25 +1,9 @@
 var sha1 = require('sha1')
+var RedisCC = require('../common/rediscc.js')
 var CONS = require('../common/constants.js')
 
 const TrustSiteDic = CONS.TrustSiteDic
 const COOKIE_KEY = CONS.COOKIE_KEY
-
-var date = new Date()
-var getRandomToken = () => sha1(date.getTime())
-var lastCookie = getRandomToken()
-
-function _CHECK_COOKIE_EXIST(req) {
-  return req.cookies.uatoken === lastCookie
-}
-
-function _CHECK_LOGIN(req, res) {
-  var checked = CONS.userAccountMap[req.query.name] && CONS.userAccountMap[req.query.name].password === req.query.pswd
-  if(checked) {
-    lastCookie = sha1(req.query.name + '|' + Math.random())
-    res.cookie(COOKIE_KEY, lastCookie)
-  }
-  return checked
-}
 
 function _CHECK_ORIGIN_TRUST(req, res) {
   if (req.headers.origin in TrustSiteDic) {
@@ -31,24 +15,44 @@ function _CHECK_ORIGIN_TRUST(req, res) {
   return false
 }
 
+function _CHECK_LOGIN(req, res) {
+  var checked = CONS.userAccountMap[req.query.name] && CONS.userAccountMap[req.query.name].password === req.query.pswd
+  if(checked) {
+    var token = sha1(req.query.name + '|' + Math.random())
+    RedisCC.client.set(COOKIE_KEY, token)
+    res.cookie(COOKIE_KEY, token)
+    return true
+  }
+  return false
+}
+
 function login(req, res) {
-  if (_CHECK_ORIGIN_TRUST(req, res)) {
-    if (_CHECK_COOKIE_EXIST(req)) {
-      res.send(true)
-    }else{
-      var responseObj = _CHECK_LOGIN(req, res)
-      res.send(responseObj)
-    }
-  }else{
+  if (_CHECK_ORIGIN_TRUST(req, res) === false) {
     res.status(403)
     res.send(null)
+    return
   }
+
+  var token = req.cookies[COOKIE_KEY]
+
+  if (!token){
+    res.send(_CHECK_LOGIN(req, res))
+    return
+  }
+
+  RedisCC.client.get(COOKIE_KEY, (err, rep) => {
+    if(rep === token){
+      res.send(true)
+    }else{
+      res.send(_CHECK_LOGIN(req, res))
+    }
+  })
+  return
 }
 
 function logout(req, res) {
   if (_CHECK_ORIGIN_TRUST(req, res)) {
-    lastCookie = null
-    //res.cookie(COOKIE_KEY, '')
+    RedisCC.client.del(COOKIE_KEY)
     res.send('cookies clear')
   }else{
     res.status(403)
