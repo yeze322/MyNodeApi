f205204573ffc5e164b084d91c7d8829479447c0
Ze Ye <zeye@microsoft.com>
2017-01-23 17:53:28 +0800

    split express app && server

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cookieParser  = require('cookie-parser')
-
-var app = express();
-app.use(bodyParser.json())
-app.use(cookieParser())
-
-var LoginApi = require('./api/login.js')
-var registerKeyListener = require('./common/registerKeyListener.js')
-var EventApi = require('./api/event.js')
-
-registerKeyListener(app, 'name')
-registerKeyListener(app, 'repo')
-registerKeyListener(app, 'pswd')
-
-app.get('/login', LoginApi.login)
-app.post('/logout', LoginApi.logout)
-app.get('/supervisor', LoginApi.supervisor)
-app.post('/eventon/:eventName', EventApi.onOpenEvent)
-app.post('/eventoff/:eventName', EventApi.onCloseEvent)
-app.get('/event/:eventName', EventApi.getEventStatus)
-
+var app = require('./app.js')
 var server = app.listen(8080, function () {
   var port = server.address().port
   console.log('Listening on port %s!', port)
 })
-
-module.exports = server
