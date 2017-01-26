2d021eea9f74134c15a26dc7915cfc5af6d6a2a8
Ze Ye <zeye@microsoft.com>
2017-01-26 14:44:11 +0800

    fix dead api request

--- common/CROSUtil.js
+++ common/CROSUtil.js
@@ -26,8 +26,8 @@ function allowCORS (req, res, next) {
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
   }
+  next()
 }
 
 module.exports = {
