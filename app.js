f6b19d4b4350277d3b3d420fd1cfa2646eaa53eb
Ze Ye <zeye@microsoft.com>
2017-01-26 14:24:03 +0800

    white list exp

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser  = require('cookie-parser')
-var cors = require('cors')
 
+var whiteList = require('./common/constants').TrustSiteDic
+function allowCORS (req, res, next) {
+  if (req.headers.origin in whiteList) {
+    res.header('ccess-Control-Allow-Origin', req.headers.origin)
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  }
+}
 var app = express();
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(allowCORS)
 
 var LoginApi = require('./api/login.js')
 var registerKeyListener = require('./common/registerKeyListener.js')
